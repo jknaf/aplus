abd06273e66143cb73f01d7b06ec1230685a41c8
jknaf <knaf@hm.edu>
2026-01-03 12:54:02 +0100

    refactor: Remove unused useState hook
    The `useState` hook was imported but never used in the `PlanningPage.tsx` component. This commit removes the unnecessary import to clean up the code.

--- pages/PlanningPage.tsx
+++ pages/PlanningPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageShell from '../components/PageShell';
 import BrochureRequestForm from '../components/BrochureRequestForm';
